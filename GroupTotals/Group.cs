c201bcb30c92ffa3ac8f9fe9dc02b47a7fb5acc2
giselleLago <gisellelago25994@gmail.com>
2020-07-18 15:29:32 +0200

    Add project files.

---
+++ GroupTotals/Group.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GroupTotals
+{
+    public class Group
+    {
+        public string GroupTotals(string[] strArr)
+        {
+            var dicc = new Dictionary<string, int>();
+            var newStrArr = new List<string>();
+            foreach (var item in strArr)
+            {
+                var convert = item.Split(':');
+                if (dicc.ContainsKey(convert[0]))
+                {
+                    dicc[convert[0]] += int.Parse(convert[1]);
+
+                }
+                else
+                {
+                    dicc.Add(convert[0], int.Parse(convert[1]));
+                }
+                
+            }
+
+            var output = dicc.Where(x => x.Value != 0).Select(x => x.Key + ":" + x.Value).ToList();
+            output.Sort();
+            return string.Join(",", output);
+             
+        }
+    }
+}
---
+++ GroupTotals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GroupTotals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Group();
+            Console.WriteLine(a.GroupTotals(new string[] { "X:-1", "Y:1", "X:-4", "B:3", "X:5" }));
+            Console.ReadKey();
+        }
+    }
+}
